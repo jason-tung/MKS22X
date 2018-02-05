d0dd9f9f76ce215b7d833300088f3763752f54c0
Jason <mickeu420@gmail.com>
2018-02-05 16:14:47 -0500

    fib works

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -2,12 +2,13 @@ public class Recursion{
     public static void main(String[] args){
 	Recursion jerry = new Recursion();
 	//System.out.println(jerry.fact(-1));
-	System.out.println(jerry.fact(0));
-	System.out.println(jerry.fact(1));
-	System.out.println(jerry.fact(2));
-	System.out.println(jerry.fact(3));
-	System.out.println(jerry.fact(4));
-	System.out.println(jerry.fact(5));
+	System.out.println(jerry.fib(0));
+	System.out.println(jerry.fib(1));
+	System.out.println(jerry.fib(2));
+	System.out.println(jerry.fib(3));
+	System.out.println(jerry.fib(4));
+	System.out.println(jerry.fib(5));
+	System.out.println(jerry.fib(10));
     }
 
     public int fact(int n){
@@ -22,6 +23,20 @@ public class Recursion{
 	    return tot;
 	}
 	return factHelper(n-1, tot * n);
+    }
+
+    public int fib(int n){
+	if (n < 0){
+	    throw new IllegalArgumentException("n must be > 0");
+	}
+	return fibHelper(n,0,1);
+    }
+
+    public int fibHelper(int n, int a, int b){
+	if (n ==0){
+	    return a;
+	}
+	return fibHelper(n-1,b,a+b);
     }
     
 }
