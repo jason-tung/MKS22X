5ebcd7260c8d3259ae9c27b7fb520b0619aba760
Jason <mickeu420@gmail.com>
2018-02-12 16:12:49 -0500

    made addqueen linear

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -24,10 +24,10 @@ public class QueenBoard{
 		    str += "\033[0;36m Q \033[0m";
 		}
 		else if (ref == 0){
-		    str += "\033[0;32m _ \033[0m";
+		    str += "\033[0;32m 0 \033[0m"; 
 		}
 		else{
-		    str += "\033[0;31m _ \033[0m";
+		    str += "\033[0;31m "+ ref  +" \033[0m";
 		}
 		if (x == size - 1){
 		    str += "\n";
@@ -39,31 +39,43 @@ public class QueenBoard{
     }
 
     private boolean addQueen(int r, int c){
-        if (board[c][r] == 0){
-	    board[c][r] = -1;
-	    for (int y = 0; y < size; y++){
-		for (int x = 0; x < size; x++){
-		    if ((x == r || y == c) && !(x == r && y == c)){
-			//System.out.println(this);
-			board[x][y] += 1;
-		    }
-		}
-	    }
+        if (board[r][c] == 0){
+	    board[r][c] = -1;
+	    for (int i = 1; i < size; i++){
+	        theDeed(r + i, c);
+		theDeed(r - i, c);
+		theDeed(r + i, c + i);
+		theDeed(r - i, c + i);
+		theDeed(r + i, c - i);
+		theDeed(r - i, c - i);
+		theDeed(r, c + i);
+		theDeed(r, c - i);
+      	    }
 	    return true;
 	}
+	//System.out.println("row : " + r + " col: " + c + " failed");
 	return false;
     }
+
+    private void theDeed(int r, int c){
+	if (r < size && r >= 0 && c < size && c >= 0){
+	    board[r][c]++;
+	}
+    }
+    
     private boolean removeQueen(int r, int c){
 	return false;
     }
 
     public static void main(String[] args){
 	QueenBoard dog = new QueenBoard(Integer.parseInt(args[0]));
+	System.out.println(dog.addQueen(2,5));
+	 System.out.println(dog);
 	System.out.println(dog.addQueen(5,5));
-	System.out.println(dog);
-	System.out.println(dog.addQueen(5,5));
-	System.out.println(dog);
-	System.out.println(dog.addQueen(4,5));
+	 System.out.println(dog);
+	 System.out.println(dog.addQueen(4,5));
+	 System.out.println(dog);
+ 	System.out.println(dog.addQueen(0,1));
 	System.out.println(dog);
     }
 	
