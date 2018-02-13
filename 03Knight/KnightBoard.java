b9de6239c1344d3f1c81ad645cafd888d6217de6
Jason <mickeu420@gmail.com>
2018-02-13 15:22:37 -0500

    deleted garbage

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,29 +1,3 @@
-// @throws IllegalArgumentException when either parameter is negative.
-// public KnightBoard(int startingRows,int startingCols)
- 
-//     Initialize the board to the correct size and make them all 0's 
-
-
-// public String toString() 
-// see format for toString below
-// blank boards display 0's as underscores 
-// you get a blank board if you never called solve or 
-// when there is no solution 
-
-// @throws IllegalStateException when the board contains non-zero values.
-// @throws IllegalArgumentException when either parameter is negative 
-//  or out of bounds.
-// public boolean solve(int startingRow, int startingCol)
-
-// @throws IllegalStateException when the board contains non-zero values. 
-// @throws IllegalArgumentException when either parameter is negative 
-//  or out of bounds.
-// public int countSolutions(int startingRow, int startingCol)
-
-// Suggestion:
-// private boolean solveH(int row ,int col, int level) 
-// // level is the # of the knight
-
 public class KnightBoard{
     public static void main(String[] dogs){
 	try{
