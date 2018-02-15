1fea0e21071180f06cc64c6522cb7c54759fb226
Jason <mickeu420@gmail.com>
2018-02-15 15:09:58 -0500

    working solve XDDDDDDDdddddddd

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -1,20 +1,9 @@
 public class KnightBoard{
     public static void main(String[] dogs){
-	try{
+
 	    KnightBoard dog = new KnightBoard(Integer.parseInt(dogs[0]), Integer.parseInt(dogs[1]));
-	    dog.board[0][2] = -1;
-	    dog.board[1][2] = 2;
-	    dog.board[2][2] = 11;
+	    System.out.println(dog.solve(0,0));
 	    System.out.println(dog);
-	}
-	catch (Exception e){
-	    System.out.println("give inputs");
-	    KnightBoard dog = new KnightBoard(10, 10);
-	    dog.board[0][2] = -1;
-	    dog.board[1][2] = 2;
-	    dog.board[2][2] = 11;
-	    System.out.println(dog);
-	}
 
     }
     
@@ -43,7 +32,7 @@ public class KnightBoard{
 		    String dog;
 		    if (ref < 10) dog = "0" + ref;
 		    else dog = ""+  ref;
-		    str += "\033[0;31m "+ dog  +" \033[0m";
+		    str += "\033[0;31m "+ "00"  +" \033[0m";
 		}
 		if (x == xsize - 1){
 		    str += "\n";
@@ -54,13 +43,41 @@ public class KnightBoard{
 	return str;
     }
 
+    public boolean addKnight(int r, int c){
+	if (r >= 0 && r < ysize && c >= 0 && c < xsize && board[r][c] == 0){
+	    board[r][c] = -1;
+	    //System.out.println(this);
+	    return true;
+	}
+	return false;
+    }
+
+    public boolean removeKnight(int r, int c){
+	if (r >= 0 && r < ysize && c >= 0 && c < xsize && board[r][c] == 0){
+	    board[r][c] = 0;
+	    return true;
+	}
+	return false;
+    }
+
     public boolean solve(int r, int c){
 	return solveH(r, c, 0);
     }
 
     public boolean solveH(int r, int c, int level){
-	return true;
+        if (level == xsize * ysize){
+	    return true;
+	}
+        if (addKnight(r,c)){
+	    return solveH(r+2, c+1, level+1) || solveH(r+2, c-1, level+1) ||
+		solveH(r-2, c+1, level+1) || solveH(r-2, c-1, level+1) ||
+		solveH(r+1, c+2, level+1) || solveH(r+1, c-2, level+1) ||
+		solveH(r-1, c+2, level+1) || solveH(r-1, c-2, level+1);
+	}
+	removeKnight(r,c);
+	return false;
     }
 
+    
     
 }
