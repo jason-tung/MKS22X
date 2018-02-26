2b62b2c9c2b4a2b43191f58b571d0ef8e1d75eda
Jason <mickeu420@gmail.com>
2018-02-26 16:43:25 -0500

    working solve

--- 03Knight/KnightBoard.java
+++ 03Knight/KnightBoard.java
@@ -4,33 +4,34 @@
 public class KnightBoard{
     public static void main(String[] dogs){
 
-	    KnightBoard dog = new KnightBoard(Integer.parseInt(dogs[0]), Integer.parseInt(dogs[1]));
-	    System.out.println(dog.solve(0,0));
-	    System.out.println(dog);
+	KnightBoard dog = new KnightBoard(Integer.parseInt(dogs[0]), Integer.parseInt(dogs[1]));
+	System.out.println(dog.solve(2,1));
+	System.out.println(dog);
 
     }
     
     public int[][] board;
-    public int xsize,ysize;
+    public int colsize,rowsize;
 
     public KnightBoard(int r, int c){
+	if (r < 0 || c < 0) throw new IllegalArgumentException();
 	board = new int[r][c];
-	xsize = c;
-	ysize = r;
+	colsize = c;
+	rowsize = r;
     }
 
     
     public String toString(){
 	String str = "\n";
-	for (int y = 0; y < ysize; y++){
-	    for (int x = 0; x < xsize; x++){
+	for (int y = 0; y < rowsize; y++){
+	    for (int x = 0; x < colsize; x++){
 		int ref = board[y][x];
 		String dog;
 		if (ref < 10) dog = "0" + ref;
 		else dog = ""+  ref;
-		str += "\033[0;31m "+ ref  +" \033[0m";
+		str += "\033[0;32m "+ dog  +" \033[0m";
 
-		if (x == xsize - 1){
+		if (x == colsize - 1){
 		    str += "\n";
 		}
 		//System.out.println(str + x + " , " + y + "\n");
@@ -39,17 +40,20 @@ public class KnightBoard{
 	return str;
     }
 
+
     public boolean addKnight(int r, int c, int level){
-	if (r >= 0 && r < ysize && c >= 0 && c < xsize && board[r][c] == 0){
+	if (r >= 0 && r < rowsize && c >= 0 && c < colsize && board[r][c] == 0){
 	    board[r][c] = level;
 	    //System.out.println(this);
 	    return true;
 	}
+	// System.out.println("row: " + r + " col: " + c + " level:" + level);
+	// System.out.println("rowsize: " + rowsize + " colsize: " + colsize);
 	return false;
     }
 
     public boolean removeKnight(int r, int c){
-	if (r >= 0 && r < ysize && c >= 0 && c < xsize && board[r][c] == 0){
+	if (r >= 0 && r < rowsize && c >= 0 && c < colsize){
 	    board[r][c] = 0;
 	    return true;
 	}
@@ -57,20 +61,31 @@ public class KnightBoard{
     }
 
     public boolean solve(int r, int c){
-	return solveH(r, c, 0);
+	if (r < 0 || c < 0) throw new IllegalArgumentException();
+	for (int[] x: board){
+	    for (int y: x){
+		if (y != 0) throw new IllegalStateException();
+	    }
+	}
+	return solveH(r, c, 1);
     }
 
     public boolean solveH(int r, int c, int level){
-        if (level == xsize * ysize){
-	    return true;
+        if (level == colsize * rowsize ){
+	    return addKnight(r,c,level);
 	}
         if (addKnight(r,c,level)){
-	    return solveH(r+2, c+1, level+1) || solveH(r+2, c-1, level+1) ||
-		solveH(r-2, c+1, level+1) || solveH(r-2, c-1, level+1) ||
-		solveH(r+1, c+2, level+1) || solveH(r+1, c-2, level+1) ||
-		solveH(r-1, c+2, level+1) || solveH(r-1, c-2, level+1);
+	    //System.out.println(this);
+	    int[][] cat = {{2,1},{2,-1},{1,2},{1,-2},{-1,2},{-1,-2},{-2,1},{-2,-1}};
+	    for (int kitten = 0; kitten < 8; kitten++){
+		if (solveH(r + cat[kitten][0], c + cat[kitten][1], level + 1)){
+		    return true;
+		    // System.out.println(removeKnight(r,c));
+		}		
+	    }
+	    removeKnight(r,c);
 	}
-	removeKnight(r,c);
+        
 	return false;
     }
 
