eb949d1386fff6e0e0bb46ecaebe548f35704fd9
Jason <mickeu420@gmail.com>
2018-03-22 15:28:21 -0400

    half done merge maybe working

--- 07Merge/Merge.java
+++ 07Merge/Merge.java
@@ -1,31 +1,15 @@
 public class Merge{
-
-	public static void merge(int[] data, int start, int middle, int end){
-		int[] lhs = new int[middle-start];
-		int[] rhs = new int[end-middle+1];
-		int lincr = 0;
-		int rincr = 0;
-		for (int i = start; i <= end; i++){
-			if (i < middle && lincr < lhs.length){
-				lhs[lincr] = data[i];
-				lincr++;
-			}
-			else{
-				rhs[rincr] = data[i];
-				rincr++;
-			}
-		}
-		lincr = 0;
-		rincr = 0;
-		for (int i = start; i <= end; i++){
-			if (lhs[lincr] < rhs[rincr] && lincr < lhs.length){
-				data[i] = lhs[lincr];
-				lincr++;
-			}
-			else{
-				data[i] = rhs[rincr];
-				rincr++;
-			}
-		}
+    //will fix later
+    public static void merge(int[] data, int[] temp, int start, int mid, int end){
+	for (int i = start; i <= end; i++){
+	    if (start <= middle && (temp[start] >= temp[end] || end <= mid)){
+		data[i] = temp[start];
+		start++;
+	    }
+	    else{
+		data[i] = temp[end];
+		end--;
+	    }
 	}
+    }
 }
