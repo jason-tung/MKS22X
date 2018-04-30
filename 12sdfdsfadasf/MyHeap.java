c01feef33d78d2da8949f123b1c74747c1183005
Jason <mickeu420@gmail.com>
2018-04-30 03:22:33 -0400

    does not work

--- 12sdfdsfadasf/MyHeap.java
+++ 12sdfdsfadasf/MyHeap.java
@@ -1,6 +1,8 @@
+import java.util.*;
+
 public class MyHeap{//<T extends Comparable<T>>{
 
-//resize swap pU pD
+//resize swap up down
 	public static boolean print = false;
 
     public static void print(Object a){
@@ -25,14 +27,14 @@ public class MyHeap{//<T extends Comparable<T>>{
     public void add(String s){
     	if (size == data.length) resize();
     	data[size] = s;
-    	pU(data.size);
+    	up(size);
     	size++;
     }
 
     public String remove(){
     	swap(0, size - 1);
     	size--;
-    	pD(0);
+    	down(0);
     	return data[size];
     }
 
@@ -43,6 +45,106 @@ public class MyHeap{//<T extends Comparable<T>>{
     	return size;
     }
 
+    public void up(int index){
+    	int yeehaw = data[index].compareTo(data[(index-1)/2]);
+    	if (typeMax && yeehaw > 0 || !typeMax && yeehaw < 0){
+    		swap(index, (index - 1) / 2);
+    		up((index - 1) / 2);
+    	}  
+    }
+
+    public void down(int index){
+    	int left = index * 2 + 1;
+    	int right = left + 1;
+    	if (typeMax){
+    		if (inBounds(left) && lessThan(index,left,data) && (!inBounds(right) || greaterThan(left,right,data))){
+    			swap(index, left);
+    			down(left);
+    		}
+    		else if(inBounds(right) && lessThan(index,right,data) && (!inBounds(left) || greaterThan(right,left,data))){
+    			swap(index, right);
+    			down(right);
+    		}
+    	}
+    	else{
+    		if (inBounds(left) && greaterThan(index,left,data) && (!inBounds(right) || lessThan(left,right,data))){
+    			swap(index, left);
+    			down(left);
+    		}
+    		else if(inBounds(right) && greaterThan(index,right,data) && (!inBounds(left) || lessThan(right,left,data))){
+    			swap(index, right);
+    			down(right);
+    		}
+    	}
+
+    }
+
+    public boolean inBounds(int index){
+    	return index < size;
+    }
+
+    public boolean greaterThan(int a, int b, String[] data){
+    	return data[a].compareTo(data[b]) > 0;
+    }
+
+    public boolean lessThan(int a, int b, String[] data){
+    	return data[a].compareTo(data[b]) < 0;
+    }
+
+    public void resize(){
+        String[] asdf = new String[2 * size];
+		for (int i = 0; i < size; i++){
+	    	asdf[i] = data[i];
+		}
+        data = asdf;	
+    }
+
+    public void swap (int a, int b){
+		String qqq = data[a];
+		data[a] = data[b];
+		data[b] = qqq;
+    }
+
+    public String toString(){
+    	String str = "";
+    	for (int i = 0; i < size; i ++){
+    		str += data[i] + " ";
+    	}
+    	return str;
+    }
+
+
+
+public static void main(String[] args) {
+    MyHeap a = new MyHeap(false);
+    String[] b = new String[26];
+    for(int i = 0; i < 26; i++){
+      int temp = 97 - i + 25;
+      char value = (char)temp;
+      a.add("" + value);
+      b[i] = "" + value;
+    }
+
+    Arrays.sort(b);
+
+    System.out.println("MyHeap: " + a);
+    System.out.println("Arrays: "+ Arrays.toString(b));
+
+    for(int i = 0; i < 26; i++){
+      //System.out.println("size: " + a.size());
+      //System.out.println("heap before: " + a.toString());
+      String temp = a.remove();
+      if(!(temp.equals(b[i]))){
+        System.out.println("there is an error");
+        System.out.println(temp);
+        System.out.println(b[i]);
+        System.out.println(a);
+      }
+      System.out.println(temp);
+      System.out.println(a);
+    }
+
+  }
 
 
 }
